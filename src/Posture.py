856a00dbaab82151eeb36ba701997a0a32168dbd
Kasia <kasia.g1607@gmail.com>
2017-11-19 01:53:11 +0100

    Łapanie wyjątku z opencv, wykrywanie kierunku na bliższej linii

--- src/Posture.py
+++ src/Posture.py
@@ -13,7 +13,6 @@ class Posture:
         self.R = randint(0, 255)
         self.G = randint(0, 255)
         self.B = randint(0, 255)
-        self.done = False
         self.state = '0'
         self.dir = None
         self.lastTime = int(time.strftime("%M%S"))
@@ -67,17 +66,12 @@ class Posture:
     def getLastTime(self):
         return self.lastTime
 
-    def setDone(self):
-        self.done = True
-
-    def getDone(self):
-        return self.done
-
-    def going_LEFT(self, line_left):
+    def going_LEFT(self, line_left, line_right):
         if len(self.tracks) >= 3:
             if self.state == '0':
                 # aktualne położenie jest mniejsze niż lewa a poprzednie było po prawej stronie lewej linii
-                if self.tracks[-1][0] < line_left and self.tracks[-2][0] >= line_left:
+                # if ((self.tracks[-1][0] < line_left and self.tracks[-2][0] >= line_left) or (self.tracks[-1][0] < line_right and self.tracks[-2][0] >= line_right)) :
+                if (self.tracks[-1][0] < line_right and self.tracks[-2][0] >= line_right):
                     self.state = '1'
                     self.dir = 'left'
                     return True
@@ -86,10 +80,11 @@ class Posture:
         else:
             return False
 
-    def going_RIGHT(self, line_right):
+    def going_RIGHT(self, line_left, line_right):
         if len(self.tracks) >= 3:
             if self.state == '0':
-                if self.tracks[-1][0] > line_right and self.tracks[-2][0] <= line_right:  # cruzo la linea
+                # if ((self.tracks[-1][0] > line_right and self.tracks[-2][0] <= line_right) or (self.tracks[-1][0] > line_left and self.tracks[-2][0] <= line_left)):
+                if (self.tracks[-1][0] > line_left and self.tracks[-2][0] <= line_left):
                     self.state = '1'
                     self.dir = 'right'
                     return True
